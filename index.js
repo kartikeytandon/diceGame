eda2b1a9ae0563d5c6133b048f6fd1ee75b49528
Kartikey Tandon <tandonkartikey11@gmail.com>
2022-06-02 18:41:36 +0530

    dice game

---
+++ index.js
@@ -0,0 +1,23 @@
+// working on the first die
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomDiceImage = "dice"+ randomNumber1 + ".png";
+var randomImageSource = "images/" + randomDiceImage;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+// working on the second die 
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+var randomDiceImag2 = "dice" + randomNumber2 + ".png";
+var randomImageSource2 = "images/" + randomDiceImage;
+var Image2 = document.querySelectorAll("img")[1];
+Image2.setAttribute("src", randomImageSource);
+
+// working of if and else conditions 
+
+if(randomNumber1 == randomNumber2) {
+    document.querySelector("h1").innerHTML="Draw";
+} else if(randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML="Player 1 wins 🚩";
+} else {
+    document.querySelector("h1").innerHTML="Player 2 wins 🚩";
+}
